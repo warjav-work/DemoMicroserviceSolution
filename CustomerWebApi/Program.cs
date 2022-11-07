c62635f8cb5f01d2e52421099d921e1ece24e7c1
DESKTOP-UKDCDPL\usuario <warjav.work@gmail.com>
2022-11-07 14:55:19 +0100

    Agregar archivos de proyecto.

---
+++ CustomerWebApi/Program.cs
@@ -0,0 +1,27 @@
+using CustomerWebApi;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+/* Database Context Dependency Injection */
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var connectionString = $"Data Source={dbHost};Initial Catalog={dbName};User ID=sa;Password={dbPassword}";
+//var connectionString = $"Data Source=DESKTOP-UKDCDPL\\WAR;Initial Catalog={dbName};Integrated Security=True;MultipleActiveResultSets=True";
+builder.Services.AddDbContext<CustomerDbContext>(options => options.UseSqlServer(connectionString));
+/* ===================================== */
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ ProductWebApi/Controllers/ProductController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductWebApi.Models;
+
+namespace ProductWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDbContext _productDbContext;
+
+        public ProductController(ProductDbContext productDbContext)
+        {
+            _productDbContext = productDbContext;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return _productDbContext.Products;
+        }
+
+        [HttpGet("{productId:int}")]
+        public async Task<ActionResult<Product>> GetById(int productId)
+        {
+            var products = await _productDbContext.Products.FindAsync(productId);
+
+            return products;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Product product)
+        {
+            await _productDbContext.Products.AddAsync(product);
+            await _productDbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Update(Product product)
+        {
+            _productDbContext.Products.Update(product);
+            await _productDbContext.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{productId:int}")]
+        public async Task<ActionResult<Product>> Delete(int productId)
+        {
+            var products = await _productDbContext.Products.FindAsync(productId);
+            _productDbContext.Products.Remove(products);
+            await _productDbContext.SaveChangesAsync();
+
+            return Ok();
+        }
+
+    }
+}
