c2be40f89031e1ab1c272a2f25bcf778ca7a3325
DESKTOP-UKDCDPL\usuario <warjav.work@gmail.com>
2022-11-11 17:07:36 +0100

    Implementar UI Project

---
+++ BlazorServerWebUI/Models/OrderDetailViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorServerWebUI.Models
+{
+    public class OrderDetailViewModel
+    {
+        public int ProductId { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ BlazorServerWebUI/Models/OrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorServerWebUI.Models
+{
+    public class OrderViewModel
+    {
+        public string OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderedOn { get; set; }
+        public List<OrderDetailViewModel> OrderDetails { get; set; }
+    }
+
+}
